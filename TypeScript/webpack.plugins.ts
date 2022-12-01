e015ebc9bf49269906c6581e2cf6489eac9f2c6b
Michael <michaelnabebe@gmail.com>
2022-11-30 22:36:41 -0500

    webpack plugins/components and feed

--- TypeScript/webpack.plugins.ts
+++ TypeScript/webpack.plugins.ts
@@ -1,10 +1,18 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+var webpack = require("webpack");
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
+  }),
+  new webpack.optimize.AggressiveSplittingPlugin({
+    minSize: 40000,
+    maxSize: 50000,
+    chunkOverhead: 0,
+    entryChunkMultiplicator: 1,
   }),
 ];
